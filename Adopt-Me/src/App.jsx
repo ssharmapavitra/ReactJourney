7f8b7024c98c7350d9664c0282fcf7ce9b13e1ca
ssharmapavitra <80170208+ssharmapavitra@users.noreply.github.com>
2023-07-27 16:59:37 +0530

    Using React Queries to fetch Details

--- Adopt-Me/src/App.jsx
+++ Adopt-Me/src/App.jsx
@@ -1,18 +1,30 @@
 import "./App.css";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Details from "./Details";
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: Infinity,
+			cacheTime: Infinity,
+		},
+	},
+});
 
 function App() {
 	return (
 		<BrowserRouter>
-			<header>
-				<Link to="/">Adopt Me!</Link>
-			</header>
-			<Routes>
-				<Route path="/details/:id" element={<Details />} />
-				<Route path="/" element={<SearchParams />} />
-			</Routes>
+			<QueryClientProvider client={queryClient}>
+				<header>
+					<Link to="/">Adopt Me!</Link>
+				</header>
+				<Routes>
+					<Route path="/details/:id" element={<Details />} />
+					<Route path="/" element={<SearchParams />} />
+				</Routes>
+			</QueryClientProvider>
 		</BrowserRouter>
 	);
 }
