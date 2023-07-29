2f20198da9f7aff9662a25319be394e5e9878f01
ssharmapavitra <80170208+ssharmapavitra@users.noreply.github.com>
2023-07-29 15:30:01 +0530

    Added Queries

--- Adopt-Me/src/fetchBreedList.js
+++ Adopt-Me/src/fetchBreedList.js
@@ -1,12 +1,17 @@
-const fetchBreedList = async ({ queryKey }) => {
+async function fetchBreedList({ queryKey }) {
 	const animal = queryKey[1];
 
-	const apiRes = await fetch(`https://pets-v2.dev-apis.com/breeds?animal=${animal}`);
-	if (!apiRes.ok) {
-		throw new Error(`An error has occured: ${apiRes.status}`);
+	if (!animal) return [];
+
+	const res = await fetch(
+		`http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+	);
+
+	if (!res.ok) {
+		throw new Error(`breeds ${animal} fetch not ok`);
 	}
 
-	return apiRes.json();
-};
+	return res.json();
+}
 
 export default fetchBreedList;
